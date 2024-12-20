f0732a3d7f1c7b6c6138832f68072f60632f3418
VietThangDev <thangdevilboss2010@gmail.com>
2024-12-21 01:47:03 +0700

    Black screen app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Helmet } from "react-helmet-async";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const handleDoubleClick = async () => {
+    try {
+      if (!document.fullscreenElement) {
+        // Kích hoạt chế độ full màn hình toàn bộ cửa sổ
+        if (document.documentElement.requestFullscreen) {
+          await document.documentElement.requestFullscreen();
+        } else if ((document.documentElement as any).webkitRequestFullscreen) {
+          await (document.documentElement as any).webkitRequestFullscreen();
+        } else if ((document.documentElement as any).msRequestFullscreen) {
+          await (document.documentElement as any).msRequestFullscreen();
+        }
+      } else {
+        // Thoát khỏi chế độ full màn hình
+        if (document.exitFullscreen) {
+          await document.exitFullscreen();
+        } else if ((document as any).webkitExitFullscreen) {
+          await (document as any).webkitExitFullscreen();
+        } else if ((document as any).msExitFullscreen) {
+          await (document as any).msExitFullscreen();
+        }
+      }
+    } catch (error) {
+      console.error("Lỗi khi chuyển đổi chế độ toàn màn hình:", error);
+    }
+  };
+
   return (
     <>
-      <div>
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundColor:"black"
+        }}
+        onDoubleClick={handleDoubleClick}
+      >
         <Helmet>
           <title>Black Screen App</title>
           <meta
@@ -12,9 +46,13 @@ function App() {
             content="Đây là trang chủ của ứng dụng Black Screen App. Tìm hiểu thêm thông tin tại đây!"
           />
         </Helmet>
+        {/* <button
+          style={{ backgroundColor: "black" }}
+          onDoubleClick={handleDoubleClick}
+        ></button> */}
       </div>
     </>
   );
-}
+};
 
 export default App;
